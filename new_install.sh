32a7471fbe93aa4a26c3694468a200d3795580d6
Kenneth Strong Jr <kenmeister9000@kens-laptop.attlocal.net>
2024-07-15 17:10:00 -0500

    script for installing my programs

---
+++ new_install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash 
+#sudo dnf install flatpak 
+#sudo pacman -S flatpak 
+#flatpak install flathub com.valvesoftware.Steam 
+#flatpak install flathub com.discordapp.Discord 
+#flatpak install flathub org.libretro.RetroArch 
+#sudo dnf install hyprland 
+#sudo pacman -S hyprland 
+#sudo dnf install waybar
+#sudo pacman -S waybar 
+#sudo dnf install alsa-lib-devel fftw3-devel pulseaudio-libs-devel libtool autoconf-archive iniparser-devel pkgconf
+#sudo dnf install cava
+#sudo pacman -S base-devel fftw alsa-lib iniparser pulseaudio autoconf-archive pkgconf
+#sudo pacaur -S cava 
+#sudo dnf install rofi 
+#sudo pacman -S rofi 
+#sudo dnf install vlc
+#sudo pacman -S vlc 
+#sudo dnf install alacritty
+#sudo pacman -S alacritty
+#sudo dnf install mpv 
+#sudo pacman -S mpv 
+#sudo dnf install gimp 
+#sudo pacman -S gimp 
+
