ab6001ec56c0f0c0f44393cd917eaa06d9473fd3
Kenneth Strong Jr <kenmeister9000@kens-laptop.attlocal.net>
2024-08-05 14:16:28 -0500

    Install script changes

--- new_install.sh
+++ new_install.sh
@@ -5,31 +5,8 @@
 #flatpak install flathub com.valvesoftware.Steam 
 #flatpak install flathub com.discordapp.Discord 
 #flatpak install flathub org.libretro.RetroArch 
-#sudo dnf install hyprland 
-#sudo pacman -S hyprland 
-#sudo dnf install waybar
-#sudo pacman -S waybar 
-#sudo dnf install alsa-lib-devel fftw3-devel pulseaudio-libs-devel libtool autoconf-archive iniparser-devel pkgconf
-#sudo dnf install cava
-#sudo pacman -S base-devel fftw alsa-lib iniparser pulseaudio autoconf-archive pkgconf
+#sudo dnf install hyprland waybar alsa-lib-devel fftw3-devel pulseaudio-libs-devel libtool autoconf-archive iniparser-devel pkgconf cava rofi vlc alacritty mpv gimp gh zsh curl 
+#sudo pacman -S hyprland waybar base-devel fftw alsa-lib iniparser pulseaudio autoconf-archive pkgconf rofi vlc alacritty mpv gimp github-cli zsh curl  
 #sudo pacaur -S cava 
-#sudo dnf install rofi 
-#sudo pacman -S rofi 
-#sudo dnf install vlc
-#sudo pacman -S vlc 
-#sudo dnf install alacritty
-#sudo pacman -S alacritty
-#sudo dnf install mpv 
-#sudo pacman -S mpv 
-#sudo dnf install gimp 
-#sudo pacman -S gimp 
-#sudo dnf install gh 
-#sudo pacman -S github-cli  
-#sudo dnf install zsh 
-#sudo pacman -S zsh
-#sudo dnf install curl 
-#sudo pacman -S curl 
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting 
 #git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
