56eea16f32ac7b8fa19289602965c24eef390b2b
kenmeister9000 <kenken5000@gmail.com>
2024-12-03 13:54:16 -0600

    Update battery.sh
    Changed line 4

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,10 +1,10 @@
 #!/bin/sh 
 export DBUS_SESSION_BUS_ADDRESS=$(< /tmp/dbus.env)
 export DISPLAY=:0  
-batstage="$(acpi -b | awk '{print $3}' | grep Charging)" 
+batstage="$(acpi -b | grep -c 'Charging')" 
 batlevel="$(acpi -b | grep -P -o '[0-9]+(?=%)')"
 
-if [[ "$batstage" == "Charging" ]]; then 
+if [[ "$batstage" == 1 ]]; then 
 dunstify -a "battery" -r 10004 -u low -i battery-symbolic -h int:value:"$batlevel" "Battery is charging ${batlevel}"
 else
 dunstify -a "battery" -r 10004 -u low -i battery-symbolic -h int:value:"$batlevel" "Battery is discharging ${batlevel}"
