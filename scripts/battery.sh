1d82f47707b3bea344800c3fbd41bd9e69059d99
kenmeister9000 <kenken5000@gmail.com>
2024-12-14 00:12:17 -0600

    Update battery.sh

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,11 +1,13 @@
 #!/bin/sh 
 export DBUS_SESSION_BUS_ADDRESS=$(< /tmp/dbus.env)
 export DISPLAY=:0  
-batstage="$(acpi -b | grep -c 'Charging')" 
+batstage="$(acpi -b | grep -c 'Charging')"
 batlevel="$(acpi -b | grep -P -o '[0-9]+(?=%)')"
-
-if [[ "$batstage" == 1 ]]; then 
-dunstify -a "battery" -r 10004 -u low -i battery-symbolic -h int:value:"$batlevel" "Battery is charging ${batlevel}%"
+batfull="$(acpi -b | grep -c 'Full')"
+if [[ "$batstage" == 1 ]]; then
+dunstify -a "battery" -r 10004 -u normal -i ac-adapter-symbolic -h int:value:"$batlevel" "Battery Charging: ${batlevel}%"
+elif [[ "$batfull" == 1 ]]; then
+dunstify -a "battery" -r 10004 -u low -i battery-symbolic -h int:value:"$batlevel" "Battery Full: ${batlevel}%"
 else
-dunstify -a "battery" -r 10004 -u low -i battery-symbolic -h int:value:"$batlevel" "Battery is discharging ${batlevel}%"
-fi 
+dunstify -a "battery" -r 10004 -u normal -i battery-symbolic -h int:value:"$batlevel" "Battery Discharging: ${batlevel}%"
+fi
