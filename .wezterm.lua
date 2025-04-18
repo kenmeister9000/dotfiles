52e5516629b557c86d18898e326af2f1ad5ad8e7
kenmeister9000 <kenken5000@gmail.com>
2025-04-18 12:08:02 -0500

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder() 
 config.color_scheme = 'Everforest Dark Medium (Gogh)'
 config.window_background_opacity = 0.7 
+config.hide_tab_bar_if_only_one_tab = true 
 config.keys = {
     { 
         key = '|',  
@@ -14,4 +15,5 @@ config.keys = {
         action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
     },
 } 
+
 return config
